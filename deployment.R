593526f565f6a6784fe750b9462c609bf5746342
moesslerm <markus.moessler@gmail.com>
2023-03-23 13:25:52 +0100

    some updates

---
+++ deployment.R
@@ -0,0 +1,11 @@
+
+getwd()
+
+setwd("C:/Users/Markus/Dropbox/Implementations/GitHub/Bookdown/topics-in-statistics-and-econometrics/_book")
+
+# In a directory with app.R
+list.files()
+
+rsconnect::writeManifest(contentCategory = "site")
+
+list.files()
